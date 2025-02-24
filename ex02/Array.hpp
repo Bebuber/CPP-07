38ab097598a346861f4152b8b1ddc9bf59a2a6a3
Betul Buber <bebuber@student.42heilbronn.de>
2025-02-24 14:38:20 +0100

    +operator[],  +main(tests)

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: bebuber <bebuber@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 16:08:10 by bebuber           #+#    #+#             */
-/*   Updated: 2025/02/22 20:32:41 by bebuber          ###   ########.fr       */
+/*   Updated: 2025/02/24 14:22:59 by bebuber          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #define ARRAY_HPP
 
 #include <cstddef>
+#include <stdexcept>
 
 template<typename T>
 class Array {
@@ -22,21 +23,24 @@ class Array {
 		Array(unsigned int n);
 		Array(const Array& other);
 		Array& operator=(const Array& other);
+		~Array();
+		
+		T& operator[](size_t index);
+		size_t getSize() const;
 	
-		~Array()
 	private:
 		T* arr;
 		size_t size;
 };
 
 template<typename T>
-Array<T>::Array() : arr(new T arr[10]), size(10) {}
+Array<T>::Array() : arr(nullptr), size(0) {}
 
 template<typename T>
-Array<T>::Array(unsigned int n) : arr(new T[n]), size(n) {}
+Array<T>::Array(unsigned int n) : arr(new T[n]()), size(n) {}
 
 template<typename T>
-Array<T>::Array(const Array<T>& other) : arr(new T[other.size]), size(other.size) {
+Array<T>::Array(const Array<T>& other) : arr(new T[other.size]()), size(other.size) {
 	for (size_t i = 0; i < size; ++i) {
 		arr[i] = other.arr[i];
 	}
@@ -57,7 +61,21 @@ Array<T>& Array<T>::operator=(const Array& other) {
 
 template<typename T>
 Array<T>::~Array() {
-	delete[] arr;
+	if (arr != nullptr)
+		delete[] arr;
+}
+
+template<typename T>
+T& Array<T>::operator[](size_t index) {
+	if (index >= size) {
+		throw std::out_of_range("Index out of range");
+	}
+	return arr[index];
+}
+
+template<typename T>
+size_t Array<T>::getSize() const {
+	return size;
 }
 
 #endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bebuber <bebuber@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 20:33:29 by bebuber           #+#    #+#             */
-/*   Updated: 2025/02/22 20:34:19 by bebuber          ###   ########.fr       */
+/*   Updated: 2025/02/24 14:34:56 by bebuber          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,28 @@
 
 int main() {
 
+	Array<int> defaultArray;
+	try {
+		std::cout << defaultArray.getSize() << std::endl;
+		std::cout << defaultArray[3] << std::endl;
+	} catch (const std::out_of_range& e) {
+		std::cerr << "Caught exception: " << e.what() << std::endl;
+	}
+
+	Array<int> sizedArray(10);
+	
+	std::cout << "sizedArray size: " << sizedArray.getSize() << std::endl;
+	std::cout << "sizedArray[5]: " << sizedArray[5] << std::endl;
+	sizedArray[5] = 42;
+	std::cout << "sizedArray[5]: " <<  sizedArray[5] << std::endl;
+
+	Array<int> another;
+	std::cout << "---------before copying---------------" << std::endl;
+	std::cout<< "another size: " << another.getSize() << std::endl;
+	another = sizedArray;
+	std::cout << "---------after copying---------------" << std::endl;
+	std::cout << "another size: " << another.getSize() << std::endl;
+	std::cout << "another[5]: " << another[5] << std::endl;
 	
 	return 0;
 }
